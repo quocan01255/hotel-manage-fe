e85d923d2442edcdaa773b5cac5bf28b07d61716
ThienAplus <thien25102@gmail.com>
2023-08-21 17:25:08 +0700

    update

--- src/Components/User/RoomCart.js
+++ src/Components/User/RoomCart.js
@@ -1,9 +1,15 @@
 import React, { useCallback, useEffect, useState, useMemo } from "react";
 import { useParams } from "react-router-dom";
 
-const RoomCart = ({ removeRoom, cart, handleNotify, totalRoomPrice }) => {
+const RoomCart = ({
+  removeRoom,
+  cart,
+  handleNotify,
+  totalRoomPrice,
+  guestCart,
+}) => {
   const [data, setData] = useState([]);
-
+  const checkLogin = localStorage.getItem("loggedIn");
   const currencyFormat = useCallback((num) => {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
   }, []);
@@ -17,44 +23,58 @@ const RoomCart = ({ removeRoom, cart, handleNotify, totalRoomPrice }) => {
   //   return cart.reduce((total, room) => total + room.price * room.quantity, 0);
   // }, [cart]);
 
+  if (!checkLogin) {
+    return (
+      <div className="mt-5 mp-5">
+        <div className="c-room-card ">
+          {guestCart.map((room) => (
+            <div key={room.id}>
+              <img
+                className="c-room-image"
+                src={room.img}
+                alt="Hình ảnh phòng"
+              />
+              <div className="c-form-details">
+                <div className="c-form-title">{room.name}</div>
+                <div className="c-room-subtitle">{room.detail}</div>
+                <div className="c-room-features">{room.description}</div>
+                <div className="c-room-rating">Quantity: {room.quantity}</div>
+                <div className="c-room-price">
+                  {currencyFormat(String(room.price))} VND
+                  <br />
+                </div>
+              </div>
+            </div>
+          ))}
+          <br />
+          <div className="c-booking-price">
+            Total price: {currencyFormat(String(totalRoomPrice))} VND
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="mp-5">
-      <div className="c-room-card mt-5 mb-5 pt-5 pb-5">
+    //có login
+    <div className="mt-5 mp-5">
+      <div className="c-room-card ">
         {cart.map((room) => (
           <div key={room.id}>
             <img className="c-room-image" src={room.img} alt="Hình ảnh phòng" />
             <div className="c-form-details">
               <div className="c-form-title">{room.name}</div>
               <div className="c-room-subtitle">{room.detail}</div>
-              <div className="c-room-features">
-                <ul>
-                  {room.description.map((item, index) => (
-                    <li key={index}>
-                      <i className="fa-solid fa-circle"></i>
-                      {item}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+              <div className="c-room-features">{room.description}</div>
               <div className="c-room-rating">Quantity: {room.quantity}</div>
               <div className="c-room-price">
-                {currencyFormat(room.price * room.quantity)}đ
-                <button
-                  className="btn btn-primary room_tab-btn"
-                  onClick={() => handleClick(room.id)}
-                >
-                  <i className="fa-solid fa-trash"></i>
-                </button>
+                {currencyFormat(room.price)} VND
               </div>
             </div>
           </div>
         ))}
         <br />
-        <div
-          className="c-booking-price"
-          style={{ fontSize: "19px", color: "red", paddingLeft: "12px" ,textAlign:"center"}}
-        >
-          Total price: {currencyFormat(String(totalRoomPrice))}đ
+        <div className="c-booking-price">
+          Total price: {currencyFormat(String(totalRoomPrice))} VND
         </div>
       </div>
     </div>
